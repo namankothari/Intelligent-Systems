ea27c0ece44f6ced4d2e34f71b83861c71aa5c6d
namankothari <namankothari25@gmail.com>
2017-03-14 17:56:16 -0400

    Update boardState.py

--- Search Algorithms/boardState.py
+++ Search Algorithms/boardState.py
@@ -1,8 +1,21 @@
-class Node:
+__author__ = 'Naman Kothari'
+"""
+Author: Naman Kothari(namankothari25@gmail.com)
 
+This object of this class represents a state in n-puzzle problem.
+
+"""
+
+class Node:
     __slots__ = 'board', 'depth', 'neighbours', 'parent', 'f', 'g', 'h'
 
     def __init__(self, board, depth=-1, parent=None):
+        """
+
+        :param board: matrix to hold the board configuration
+        :param depth: depth of the state with respect to start state
+        :param parent: parent state from which this state is generated
+        """
         self.board = board
         self.depth = depth
         self.parent = parent
@@ -12,6 +25,10 @@ class Node:
         return self.f < other.f
 
     def calculateHeuristic(self):
+        """
+        Computes the value of f, g, h required for A* and IDA*
+        :return: None
+        """
         if self.parent is not None:
             self.g = self.parent.g + 1
         else:
@@ -20,6 +37,10 @@ class Node:
         self.f = self.g + self.h
 
     def getManhattan(self):
+        """
+        Calculates heuristic cost of current state by using manhattan distance
+        :return: heuristic cost
+        """
         steps = 0
         for index in range(1, len(self.board)**2):
             iGoal, jGoal = index / len(self.board), index % len(self.board)
@@ -36,9 +57,18 @@ class Node:
         return steps
 
     def getBoard(self):
+        """
+
+        :return: board configuration of current state
+        """
         return self.board
 
     def getNeighbours(self):
+        """
+        Generates the neighbours of the current state
+        :return: neighbouring states of current state
+        """
+
         iBlank, jBlank = self.getBlankIndex()
 
         upNeighbour = self.getUpNeighbour(iBlank, jBlank)
@@ -60,6 +90,12 @@ class Node:
         return self.neighbours
 
     def getLeftNeighbour(self, iBlank, jBlank):
+        """
+
+        :param iBlank: row of blank tile
+        :param jBlank: col of blank tile
+        :return: left neighbour of the current state
+        """
         if jBlank == 0:
             return -1
 
@@ -77,6 +113,12 @@ class Node:
         return nextBoard
 
     def getRightNeighbour(self, iBlank, jBlank):
+        """
+
+        :param iBlank: row of blank tile
+        :param jBlank: col of blank tile
+        :return: right neighbour of the current state
+        """
         if jBlank == len(self.board) - 1:
             return -1
 
@@ -93,6 +135,12 @@ class Node:
         return nextBoard
 
     def getUpNeighbour(self, iBlank, jBlank):
+        """
+
+        :param iBlank: row of blank tile
+        :param jBlank: col of blank tile
+        :return: up neighbour of the current state
+        """
         if iBlank == 0:
             return -1
 
@@ -109,6 +157,12 @@ class Node:
         return nextBoard
 
     def getDownNeighbour(self, iBlank, jBlank):
+        """
+
+        :param iBlank: row of blank tile
+        :param jBlank: col of blank tile
+        :return: down neighbour of the current state
+        """
         if iBlank == len(self.board) - 1:
             return -1
 
@@ -125,9 +179,11 @@ class Node:
         return nextBoard
 
     def getBlankIndex(self):
+        """
+
+        :return: row and column of the blank tile
+        """
         for i in range(len(self.board)):
             for j in range(len(self.board)):
                 if self.board[i][j] == 0:
                     return i, j
-                
-                
